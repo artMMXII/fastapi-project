ba6c162887f6cf37d0f538e8e198555df2a87921
Khasiev Arthur <hasiev_artur@mail.ru>
2026-06-28 21:46:56 +0700

    add parameters to STaskAdd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 app = FastAPI(
--- schemas.py
+++ schemas.py
@@ -1,6 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class STaskAdd(BaseModel):
-    name: str
-    description: str | None = None
-    is_done: bool = False
+    name: str = Field(
+        ...,
+        min_length=2,
+        max_length=100,
+    )
+
+    priority: int = Field(
+        default=1,
+        ge=1,
+        le=5
+    )
+
+    description: str | None = Field(
+        default=None,
+        max_length=300,
+        title="Краткое описание",
+        description='Подробное описание задачи'
+    )
