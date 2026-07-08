9c145e87b9adebf1591647957c70182fd708a3b1
Khasiev Arthur <hasiev_artur@mail.ru>
2026-07-08 14:36:53 +0700

    add HTTPException

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, HTTPException, status
 from schemas import STaskAdd, STask
 
 app = FastAPI()
 
-tasks = []
+tasks = [{"id": 1, "name": "Тест"}, {"id": 2, "name": "Код"}]
 
 
 @app.post("/tasks", response_model=STask, status_code=status.HTTP_201_CREATED)
@@ -14,3 +14,23 @@ async def create_task(task: STaskAdd):
     tasks.append(task_dict)
 
     return task_dict
+
+
+@app.get("/tasks/{task_id}")
+async def get_task(task_id: int):
+    # Пытаемся найти задачу (упрощенная логика поиска)
+    current_task = None
+    for t in tasks:
+        if t["id"] == task_id:
+            current_task = t
+            break
+
+    # Если задача не найдена (переменная осталась None)
+    if current_task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Задача с ID {task_id} не найдена"
+        )
+
+    return current_task
+
